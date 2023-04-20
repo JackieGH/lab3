833e2e3beba2d38c4d6df3c051eb1b7b438f5f93
Jackie Maldonado <118325713+JackieGH@users.noreply.github.com>
2023-04-19 18:36:53 -0700

    Updated lab progress

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,25 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace1() {
+    int[] input2 = {1,2,3};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{3,2,1}, input2);
+  }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input3 = {10, 10, 10, 1};
+    double output = ArrayExamples.averageWithoutLowest(input3);
+    assertEquals(10.0, output, 0.1);
+  }
+
+  @Test
+  public void testAverageWithoutLowest1() {
+    double[] input3 = {10, 10.2};
+    double output = ArrayExamples.averageWithoutLowest(input3);
+    assertEquals(10.2, output, 0.1);
+  }
 }
